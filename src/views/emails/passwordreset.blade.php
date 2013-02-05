0525c92345607bb0444abaea2db370764821f3a3
Zizaco <zizaco@gmail.com>
2013-02-05 06:59:31 -0200

    Lang term fix in Password reset view

--- src/views/emails/passwordreset.blade.php
+++ src/views/emails/passwordreset.blade.php
@@ -2,6 +2,6 @@
 
 <p>{{ Lang::get('confide::confide.email.password_reset.greetings', array( 'name' => $user->username)); }},</p>
 
-<p>{{ Lang::get('confide::confide.email.password_reset.greetings', array( 'password' => $new_password)); }}</p>
+<p>{{ Lang::get('confide::confide.email.password_reset.body', array( 'password' => $new_password)); }}</p>
 
 <p>{{ Lang::get('confide::confide.email.password_reset.farewell'); }}</p>
