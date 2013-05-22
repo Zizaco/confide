c9823d2f0657e0db1f9ccb7cce4919a9598e8243
Andrew Elkins <andrew@andrewelkins.com>
2013-05-22 11:36:02 -0700

    Fixes #72 for good this time

--- src/Zizaco/Confide/ConfideUser.php
+++ src/Zizaco/Confide/ConfideUser.php
@@ -331,6 +331,32 @@ class ConfideUser extends Ardent implements UserInterface {
     }
 
     /**
+     * Parses the two given users and compares the unique fields.
+     * @param $oldUser
+     * @param $updatedUser
+     * @param array $rules
+     */
+    public function prepareRules($oldUser, $updatedUser, $rules=array())
+    {
+        if(empty($rules)) {
+            $rules = $this->getRules();
+        }
+
+        foreach($rules as $rule => $validation) {
+            // get the rules with unique.
+            if (strpos($validation, 'unique')) {
+                // Compare old vs new
+                if($oldUser->$rule != $updatedUser->$rule) {
+                    // Set update rule to creation rule
+                    $updateRules = $this->getUpdateRules();
+                    $updateRules[$rule] = $validation;
+                    $this->setUpdateRules($updateRules);
+                }
+            }
+        }
+    }
+
+    /**
      * Add the namespace 'confide::' to view hints.
      * this makes possible to send emails using package views from
      * the command line.
@@ -391,6 +417,11 @@ class ConfideUser extends Ardent implements UserInterface {
     public function getUpdateRules()
     {
         return $this->updateRules;
+    }
+
+    public function getRules()
+    {
+        return self::$rules;
     }
 
     public function setUpdateRules($set)
