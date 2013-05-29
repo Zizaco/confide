6bf4bb45b51982ec61d48c509f956a9bb3c461fd
Andrew <github@andrewelkins.com>
2013-05-28 18:53:31 -0600

    Properly protects against the false column name

--- src/Zizaco/Confide/Confide.php
+++ src/Zizaco/Confide/Confide.php
@@ -100,7 +100,7 @@ class Confide
             $user = $this->model()
                 ->getUserFromCredsIdentity($credentials, $identity_columns);
 
-            $passwordField =  (is_null($this->app['config']->get('confide::password_field'))) ? $this->app['config']->get('confide::password_field') : 'password';
+            $passwordField =  (!is_null($this->app['config']->get('confide::password_field'))) ? $this->app['config']->get('confide::password_field') : 'password';
 
             if( ! is_null($user) and ($user->confirmed or !$confirmed_only ) and
                 $this->app['hash']->check($credentials['password'], $user->$passwordField) )
