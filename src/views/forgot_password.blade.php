9776136f78104a19239c6b4fde7ae49a5766de7b
Dan Sullivan <dan@embu.co.uk>
2013-08-28 11:00:21 +0100

    Add Twitter Bootstrap 3 support in forms

--- src/views/forgot_password.blade.php
+++ src/views/forgot_password.blade.php
@@ -1,15 +1,18 @@
 <form method="POST" action="{{ (Confide::checkAction('UserController@do_forgot_password')) ?: URL::to('/user/forgot') }}" accept-charset="UTF-8">
     <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
 
-    <label for="email">{{{ Lang::get('confide::confide.e_mail') }}}</label>
-    <div class="input-append">
-        <input placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
-
-        <input class="btn" type="submit" value="{{{ Lang::get('confide::confide.forgot.submit') }}}">
+    <div class="form-group">
+        <label for="email">{{{ Lang::get('confide::confide.e_mail') }}}</label>
+        <div class="input-append input-group">
+            <input class="form-control" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
+            <span class="input-group-btn">
+                <input class="btn btn-default" type="submit" value="{{{ Lang::get('confide::confide.forgot.submit') }}}">
+            </span>
+        </div>
     </div>
 
     @if ( Session::get('error') )
-        <div class="alert alert-error">{{{ Session::get('error') }}}</div>
+        <div class="alert alert-error alert-danger">{{{ Session::get('error') }}}</div>
     @endif
 
     @if ( Session::get('notice') )
