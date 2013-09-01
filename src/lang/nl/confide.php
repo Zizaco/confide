8c86693ef2f9c8c2d2ed0a26ad10601de6421f4b
JeroenG <Jeroen-G@users.noreply.github.com>
2013-09-01 21:25:45 +0200

    Dutch translation

---
+++ src/lang/nl/confide.php
@@ -0,0 +1,62 @@
+<?php
+
+return array(
+
+    'username' => 'Gebruikersnaam',
+    'password' => 'Wachtwoord',
+    'password_confirmation' => 'Bevestig wachtwoord',
+    'e_mail' => 'E-mail',
+    'username_e_mail' => 'Gebruikersnaam of e-mail',
+
+    'signup' => array(
+        'title' => 'Registratie',
+        'desc' => 'Nieuw account aanmaken',
+        'confirmation_required' => 'Bevestiging is verplicht',
+        'submit' => 'Registreren',
+    ),
+
+    'login' => array(
+        'title' => 'Inloggen',
+        'desc' => 'Vul je gebruikersnaam en wachtwoord in',
+        'forgot_password' => '(wachtwoord vergeten)',
+        'remember' => 'Onthouden',
+        'submit' => 'Inloggen',
+    ),
+
+    'forgot' => array(
+        'title' => 'Wachtwoord vergeten',
+        'submit' => 'Doorgaan',
+    ),
+
+    'alerts' => array(
+        'account_created' => 'Uw account is met succes aangemaakt. Controleer uw e-mail voor de instructies voor het activeren van uw account.',
+        'too_many_attempts' => 'Teveel pogingen, probeer het nogmaals over enkele minuten.',
+        'wrong_credentials' => 'Onjuist e-mailadres, gebruikersnaam of wachtwoord.',
+        'not_confirmed' => 'Uw account is nog niet geactiveerd, controleer uw e-mail voor de activatiegegevens.',
+        'confirmation' => 'Uw account is geactiveerd! U kunt nu inloggen.',
+        'wrong_confirmation' => 'Onjuiste activatie.',
+        'password_forgot' => 'De informatie is naar uw e-mail verzonden.',
+        'wrong_password_forgot' => 'Gebruiker kon niet gevonden worden.',
+        'password_reset' => 'Your password has been changed successfully.',
+        'wrong_password_reset' => 'Onjuist wachtwoord, probeer het nog eens.',
+        'wrong_token' => 'De reset token is onjuist.',
+        'duplicated_credentials' => 'De ingevulde gegevens zijn al in gebruik.',
+    ),
+
+    'email' => array(
+        'account_confirmation' => array(
+            'subject' => 'Account activatie',
+            'greetings' => 'Beste :name',
+            'body' => 'Gebruik onderstaande link om uw account te activeren.',
+            'farewell' => 'Met vriendelijke groeten',
+        ),
+
+        'password_reset' => array(
+            'subject' => 'Wachtwoord reset',
+            'greetings' => 'Beste :name',
+            'body' => 'Gebruik onderstaande link om uw wachtwoord te veranderen.',
+            'farewell' => 'Met vriendelijke groeten',
+        ),
+    ),
+
+);
