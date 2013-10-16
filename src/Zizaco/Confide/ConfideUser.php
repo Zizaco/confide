7455f0bfe7d200686e658689c0a356abe30bfde6
Andrew <github@andrewelkins.com>
2013-10-16 16:13:21 -0700

    Displays correct validation error #124
    Thanks Eugene

--- src/Zizaco/Confide/ConfideUser.php
+++ src/Zizaco/Confide/ConfideUser.php
@@ -181,6 +181,7 @@ class ConfideUser extends Ardent implements UserInterface {
         }
         else
         {
+            $this->validate();
             $this->validationErrors->add(
                 'duplicated',
                 static::$app['translator']->get('confide::confide.alerts.duplicated_credentials')
