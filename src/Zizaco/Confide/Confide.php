294b120ae21615b8b9d698e5e362c56cfca432ef
Zizaco <zizaco@gmail.com>
2014-01-31 23:38:33 -0200

    Added logAttempt method to Confide main class in order to actually log in.

--- src/Zizaco/Confide/Confide.php
+++ src/Zizaco/Confide/Confide.php
@@ -74,8 +74,65 @@ class Confide
      * @param string $code
      * @return bool Success
      */
-    public function confirm( $code )
+    public function confirm($code)
     {
         return $this->repo->confirmByCode($code);
+    }
+
+    /**
+     * Attempt to log a user into the application with
+     * password and identity field(s), usually email or username.
+     *
+     * @param  array $input Array containing at least 'username' or 'email' and 'password'. Optionally the 'remember' boolean.
+     * @param  bool $mustBeConfirmed If true, the user must have confirmed his email account in order to log-in.
+     * @return boolean Success
+     */
+    public function logAttempt($input, $mustBeConfirmed = true)
+    {
+        $remember = $this->extractRememberFromArray($input);
+        $emailOrUsername = $this->extractIdentityFromArray($input);
+        $user = $this->repo->getUserByEmailOrUsername($emailOrUsername);
+
+        if ($user) {
+            if (! $user->confirmed && $mustBeConfirmed )
+                return false;
+
+            $correctPassword = $this->app['hash']->check(
+                isset($input['password']) ? $input['password'] : false,
+                $user->password
+            );
+
+            if (! $correctPassword)
+                return false;
+
+            $this->app['auth']->login($user, $remember);
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function extractRememberFromArray($input)
+    {
+        if (isset($input['remember'])) {
+            $remember = $input['remember'];
+        } else {
+            $remember = false;
+        }
+
+        return $remember;
+    }
+
+    protected function extractIdentityFromArray($input)
+    {
+        if (isset($input['email'])) {
+            $emailOrUsername = $input['email'];
+        } elseif (isset($input['username'])) {
+            $emailOrUsername = $input['username'];
+        } else {
+            return false;
+        }
+
+        return $emailOrUsername;
     }
 }
--- tests/Zizaco/Confide/ConfideTest.php
+++ tests/Zizaco/Confide/ConfideTest.php
@@ -104,4 +104,397 @@ class ConfideTest extends PHPUnit_Framework_TestCase
         */
         $this->assertTrue($confide->confirm($code));
     }
+
+    public function testShouldDoLogAttempt()
+    {
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
+        $app = [];
+        $app['auth'] = m::mock('Auth');
+        $app['hash'] = m::mock('Hash');
+        $repo = m::mock('Zizaco\Confide\RepositoryInterface');
+        $passService = m::mock('Zizaco\Confide\PasswordServiceInterface');
+
+        $confide = m::mock(
+            'Zizaco\Confide\Confide'.
+            '[extractRememberFromArray,extractIdentityFromArray]',
+            [$repo, $passService, $app]
+        );
+        $confide->shouldAllowMockingProtectedMethods();
+
+        $user = m::mock('_mockedUser');
+        $user->confirmed = true;
+        $user->email = 'someone@somewhere.com';
+        $user->password = 'secret';
+
+        $remember = true;
+        $input = [
+            'email'=>$user->email,
+            'password'=>$user->password,
+            'remember'=>$remember
+        ];
+
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
+        $confide->shouldReceive('extractRememberFromArray')
+            ->with($input)->andReturn(true);
+
+        $confide->shouldReceive('extractIdentityFromArray')
+            ->with($input)->andReturn($user->email);
+
+        $repo->shouldReceive('getUserByEmailOrUsername')
+            ->once()->with('someone@somewhere.com')
+            ->andReturn($user);
+
+        $app['hash']->shouldReceive('check')
+            ->once()->with($user->password, $user->password)
+            ->andReturn(true);
+
+        $app['auth']->shouldReceive('login')
+            ->once()->with($user, $remember)
+            ->andReturn(true);
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+        $this->assertTrue($confide->logAttempt($input));
+    }
+
+    public function testShouldFailLogAttemptIfUserNotFound()
+    {
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
+        $app = [];
+        $app['auth'] = m::mock('Auth');
+        $app['hash'] = m::mock('Hash');
+        $repo = m::mock('Zizaco\Confide\RepositoryInterface');
+        $passService = m::mock('Zizaco\Confide\PasswordServiceInterface');
+
+        $confide = m::mock(
+            'Zizaco\Confide\Confide'.
+            '[extractRememberFromArray,extractIdentityFromArray]',
+            [$repo, $passService, $app]
+        );
+        $confide->shouldAllowMockingProtectedMethods();
+
+        $user = m::mock('_mockedUser');
+        $user->confirmed = true;
+        $user->email = 'someone@somewhere.com';
+        $user->password = 'secret';
+
+        $remember = true;
+        $input = [
+            'email'=>$user->email,
+            'password'=>$user->password,
+            'remember'=>$remember
+        ];
+
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
+        $confide->shouldReceive('extractRememberFromArray')
+            ->with($input)->andReturn(true);
+
+        $confide->shouldReceive('extractIdentityFromArray')
+            ->with($input)->andReturn($user->email);
+
+        $repo->shouldReceive('getUserByEmailOrUsername')
+            ->once()->with('someone@somewhere.com')
+            ->andReturn(false);
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+        $this->assertFalse($confide->logAttempt($input));
+    }
+
+    public function testShouldFailLogAttemptIfUserNotConfirmed()
+    {
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
+        $app = [];
+        $app['auth'] = m::mock('Auth');
+        $app['hash'] = m::mock('Hash');
+        $repo = m::mock('Zizaco\Confide\RepositoryInterface');
+        $passService = m::mock('Zizaco\Confide\PasswordServiceInterface');
+
+        $confide = m::mock(
+            'Zizaco\Confide\Confide'.
+            '[extractRememberFromArray,extractIdentityFromArray]',
+            [$repo, $passService, $app]
+        );
+        $confide->shouldAllowMockingProtectedMethods();
+
+        $user = m::mock('_mockedUser');
+        $user->confirmed = false;
+        $user->email = 'someone@somewhere.com';
+        $user->password = 'secret';
+
+        $remember = true;
+        $input = [
+            'email'=>$user->email,
+            'password'=>$user->password,
+            'remember'=>$remember
+        ];
+
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
+        $confide->shouldReceive('extractRememberFromArray')
+            ->with($input)->andReturn(true);
+
+        $confide->shouldReceive('extractIdentityFromArray')
+            ->with($input)->andReturn($user->email);
+
+        $repo->shouldReceive('getUserByEmailOrUsername')
+            ->once()->with('someone@somewhere.com')
+            ->andReturn($user);
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+        $this->assertFalse($confide->logAttempt($input));
+    }
+
+    public function testShouldFailLogAttemptIfWrongPass()
+    {
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
+        $app = [];
+        $app['auth'] = m::mock('Auth');
+        $app['hash'] = m::mock('Hash');
+        $repo = m::mock('Zizaco\Confide\RepositoryInterface');
+        $passService = m::mock('Zizaco\Confide\PasswordServiceInterface');
+
+        $confide = m::mock(
+            'Zizaco\Confide\Confide'.
+            '[extractRememberFromArray,extractIdentityFromArray]',
+            [$repo, $passService, $app]
+        );
+        $confide->shouldAllowMockingProtectedMethods();
+
+        $user = m::mock('_mockedUser');
+        $user->confirmed = true;
+        $user->email = 'someone@somewhere.com';
+        $user->password = 'secret';
+
+        $remember = true;
+        $input = [
+            'email'=>$user->email,
+            'password'=>$user->password,
+            'remember'=>$remember
+        ];
+
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
+        $confide->shouldReceive('extractRememberFromArray')
+            ->with($input)->andReturn(true);
+
+        $confide->shouldReceive('extractIdentityFromArray')
+            ->with($input)->andReturn($user->email);
+
+        $repo->shouldReceive('getUserByEmailOrUsername')
+            ->once()->with('someone@somewhere.com')
+            ->andReturn($user);
+
+        $app['hash']->shouldReceive('check')
+            ->once()->with($user->password, $user->password)
+            ->andReturn(false);
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+        $this->assertFalse($confide->logAttempt($input));
+    }
+
+    public function testShouldDoLogAttemptIfNotConfirmed()
+    {
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
+        $app = [];
+        $app['auth'] = m::mock('Auth');
+        $app['hash'] = m::mock('Hash');
+        $repo = m::mock('Zizaco\Confide\RepositoryInterface');
+        $passService = m::mock('Zizaco\Confide\PasswordServiceInterface');
+
+        $confide = m::mock(
+            'Zizaco\Confide\Confide'.
+            '[extractRememberFromArray,extractIdentityFromArray]',
+            [$repo, $passService, $app]
+        );
+        $confide->shouldAllowMockingProtectedMethods();
+
+        $user = m::mock('_mockedUser');
+        $user->confirmed = false;
+        $user->email = 'someone@somewhere.com';
+        $user->password = 'secret';
+
+        $remember = true;
+        $input = [
+            'email'=>$user->email,
+            'password'=>$user->password,
+            'remember'=>$remember
+        ];
+
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
+        $confide->shouldReceive('extractRememberFromArray')
+            ->with($input)->andReturn(true);
+
+        $confide->shouldReceive('extractIdentityFromArray')
+            ->with($input)->andReturn($user->email);
+
+        $repo->shouldReceive('getUserByEmailOrUsername')
+            ->once()->with('someone@somewhere.com')
+            ->andReturn($user);
+
+        $app['hash']->shouldReceive('check')
+            ->once()->with($user->password, $user->password)
+            ->andReturn(true);
+
+        $app['auth']->shouldReceive('login')
+            ->once()->with($user, $remember)
+            ->andReturn(true);
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+        $this->assertTrue($confide->logAttempt($input, false));
+    }
+
+    public function testShouldExtractRememberFromArray()
+    {
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
+        $app = [];
+        $repo = m::mock('Zizaco\Confide\RepositoryInterface');
+        $passService = m::mock('Zizaco\Confide\PasswordServiceInterface');
+
+        $confide = m::mock(
+            'Zizaco\Confide\Confide'.
+            '[extractRememberFromArray]',
+            [$repo, $passService, $app]
+        );
+        $confide->shouldAllowMockingProtectedMethods();
+
+        $inputWithoutRemember = [];
+        $inputWithRemember = [
+            'remember' => true
+        ];
+
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
+        $confide->shouldReceive('extractRememberFromArray')
+            ->passthru();
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+        $this->assertTrue(
+            $confide->extractRememberFromArray($inputWithRemember)
+        );
+        $this->assertFalse(
+            $confide->extractRememberFromArray($inputWithoutRemember)
+        );
+    }
+
+    public function testShouldExtractIdentityFromArray()
+    {
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
+        $app = [];
+        $repo = m::mock('Zizaco\Confide\RepositoryInterface');
+        $passService = m::mock('Zizaco\Confide\PasswordServiceInterface');
+
+        $confide = m::mock(
+            'Zizaco\Confide\Confide'.
+            '[extractIdentityFromArray]',
+            [$repo, $passService, $app]
+        );
+        $confide->shouldAllowMockingProtectedMethods();
+
+        $emptyId = ['garbage'=>'dontNeed'];
+        $userId  = ['username' => 'someone', 'garbage'=>'dontNeed'];
+        $emailId = ['email' => 'someone@somewhere.com', 'garbage'=>'dontNeed'];
+        $bothId  = ['email' => 'someone@somewhere.com', 'username' => 'someone', 'garbage'=>'dontNeed'];
+
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
+        $confide->shouldReceive('extractIdentityFromArray')
+            ->passthru();
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+        $this->assertEquals(
+            false,
+            $confide->extractIdentityFromArray($emptyId)
+        );
+        $this->assertEquals(
+            'someone',
+            $confide->extractIdentityFromArray($userId)
+        );
+        $this->assertEquals(
+            'someone@somewhere.com',
+            $confide->extractIdentityFromArray($emailId)
+        );
+        $this->assertEquals(
+            'someone@somewhere.com',
+            $confide->extractIdentityFromArray($bothId)
+        );
+    }
 }
