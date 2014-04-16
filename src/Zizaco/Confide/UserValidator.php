4d479423818e60c78a3e3d43f795ae4e646480eb
Zizaco <zizaco@gmail.com>
2014-04-16 05:56:15 -0300

    Added initial implementation of UserValidator

---
+++ src/Zizaco/Confide/UserValidator.php
@@ -0,0 +1,38 @@
+<?php namespace Zizaco\Confide;
+
+/**
+ * This is the default validator used by ConfideUser. You may overwrite this
+ * class and implement your own validator by creating a class that implements
+ * the `UserValidatorInterface` and by registering that class in IoC container
+ * as 'confide.user_validator'.
+ *
+ * This validator will look for the basic fields (username, email,
+ * password), and if the user is unique.
+ *
+ * In order to use a custom validator:
+ *     // MyOwnValidator.php
+ *     class MyOwnValidator implements UserValidatorInterface {
+ *         ...
+ *     }
+ *
+ *     // routes.php
+ *     ...
+ *     App::bind('confide.user_validator', 'MyOwnValidator');
+ *
+ * @see \Zizaco\Confide\UserValidator
+ * @license MIT
+ * @package  Zizaco\Confide
+ */
+class UserValidator implements UserValidatorInterface {
+
+    /**
+     * Validates the given user. Should check if all the fields are correctly
+     * @param  ConfideUserInterface $user Instance to be tested
+     * @return boolean                    True if the $user is valid
+     */
+    public function validate(ConfideUserInterface $user)
+    {
+        return true;
+    }
+
+}
---
+++ src/Zizaco/Confide/UserValidatorInterface.php
@@ -0,0 +1,21 @@
+<?php namespace Zizaco\Confide;
+
+/**
+ * Interface that declares the methods that must be
+ * present in the UserValidator that is going to be used
+ * by ConfideUser
+ *
+ * @see \Zizaco\Confide\UserValidator
+ * @license MIT
+ * @package  Zizaco\Confide
+ */
+interface UserValidatorInterface
+{
+    /**
+     * Validates the given user. Should check if all the fields are correctly
+     * and may check other stuff too, like if the user is unique.
+     * @param  ConfideUserInterface $user Instance to be tested
+     * @return boolean                    True if the $user is valid
+     */
+    public function validate(ConfideUserInterface $user);
+}
