97d43f5fd6427d067d81dddc9b63728d1d51bed7
Zizaco <zizaco@gmail.com>
2014-04-18 21:14:48 -0300

    Added actual implementation of UserValidator::validatePassword method in order to to allow password creation and password update

--- src/Zizaco/Confide/UserValidator.php
+++ src/Zizaco/Confide/UserValidator.php
@@ -1,5 +1,7 @@
 <?php namespace Zizaco\Confide;
 
+use Illuminate\Support\Facades\App as App;
+
 /**
  * This is the default validator used by ConfideUser. You may overwrite this
  * class and implement your own validator by creating a class that implements
@@ -26,14 +28,70 @@
 class UserValidator implements UserValidatorInterface {
 
     /**
+     * Confide repository instance
+     *
+     * @var \Zizaco\Confide\RepositoryInterface
+     */
+    public $repo;
+
+    /**
+     * Validation rules for this Validator.
+     *
+     * @var array
+     */
+    protected $rules = [
+        'create' => [
+            'username' => 'required|alpha_dash',
+            'email'    => 'required|email',
+            'password' => 'required|min:4',
+        ],
+        'update' => [
+            'username' => 'required|alpha_dash',
+            'email'    => 'required|email',
+            'password' => 'required|min:4',
+        ]
+    ];
+
+    /**
      * Validates the given user. Should check if all the fields are correctly
      * @param  ConfideUserInterface $user Instance to be tested
      * @return boolean                    True if the $user is valid
      */
-    public function validate(ConfideUserInterface $user)
+    public function validate(ConfideUserInterface $user, $ruleset = 'create')
     {
-        unset($user->password_confirmation);
-        $user->password = \Hash::make($user->password);
+        // Set the $repo as a ConfideRepository object
+        $this->repo = App::make('confide.repository');
+
+        // Validate object
+        $result = $this->validatePassword($user) &&
+            $this->isUnique($user) &&
+            $this->validateFields($user);
+
+        return $result;
+    }
+
+    /**
+     * Validates the password and password_confirmation of the given
+     * user
+     * @param  ConfideUserInterface $user
+     * @return boolean  True if password is valid
+     */
+    public function validatePassword(ConfideUserInterface $user)
+    {
+        $hash = App::make('hash');
+
+        if($user->getOriginal('password') != $user->password) {
+            if ($user->password == $user->password_confirmation) {
+
+                // Hashes password and unset password_confirmation field
+                $user->password = $hash->make($user->password);
+                unset($user->password_confirmation);
+
+                return true;
+            } else {
+                return false;
+            }
+        }
 
         return true;
     }
--- tests/Zizaco/Confide/UserValidatorTest.php
+++ tests/Zizaco/Confide/UserValidatorTest.php
@@ -2,6 +2,7 @@
 
 use Mockery as m;
 use PHPUnit_Framework_TestCase;
+use Illuminate\Support\Facades\App as App;
 
 class UserValidatorTest extends PHPUnit_Framework_TestCase
 {
@@ -13,5 +14,110 @@ class UserValidatorTest extends PHPUnit_Framework_TestCase
     public function tearDown()
     {
         m::close();
+    }
+
+    public function testShouldValidate()
+    {
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
+       $repo = m::mock('Zizaco\Confide\EloquentRepository');
+
+        App::shouldReceive('make')
+            ->with('confide.repository')
+            ->andReturn($repo);
+
+        $validator = m::mock(
+            'Zizaco\Confide\UserValidator'.
+            '[validatePassword,isUnique,validateFields]'
+        );
+        $validator->shouldAllowMockingProtectedMethods();
+
+        $user = m::mock('Zizaco\Confide\ConfideUserInterface');
+
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
+        $validator->shouldReceive('validatePassword')
+            ->once()->andReturn(true);
+
+        $validator->shouldReceive('isUnique')
+            ->once()->andReturn(true);
+
+        $validator->shouldReceive('validateFields')
+            ->once()->andReturn(true);
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+        $this->assertTrue($validator->validate($user));
+    }
+
+    public function testShouldValidatePassword()
+    {
+        /*
+        |------------------------------------------------------------
+        | Set
+        |------------------------------------------------------------
+        */
+        $repo = m::mock('Zizaco\Confide\EloquentRepository');
+        $hash = m::mock('Hash');
+
+        App::shouldReceive('make')
+            ->with('confide.repository')
+            ->andReturn($repo);
+
+        App::shouldReceive('make')
+            ->with('hash')
+            ->andReturn($hash);
+
+        $validator = new UserValidator;
+
+        $userA = m::mock('Zizaco\Confide\ConfideUserInterface');
+        $userA->password              = 'foo123';
+        $userA->password_confirmation = 'foo123';
+
+        $userB = m::mock('Zizaco\Confide\ConfideUserInterface');
+        $userB->password              = 'foo123';
+        $userB->password_confirmation = 'foo456';
+
+        $userC = m::mock('Zizaco\Confide\ConfideUserInterface');
+        $userC->password = '$2y$10$8PqTle4VGODMbjFbpIe.vOISth8qAaXlO7CAi4HNneqe37Jy1gGRO';
+
+        /*
+        |------------------------------------------------------------
+        | Expectation
+        |------------------------------------------------------------
+        */
+        $userA->shouldReceive('getOriginal')
+            ->once()->with('password')
+            ->andReturn('');
+
+        $userB->shouldReceive('getOriginal')
+            ->once()->with('password')
+            ->andReturn('old_pass');
+
+        $userC->shouldReceive('getOriginal')
+            ->once()->with('password')
+            ->andReturn('$2y$10$8PqTle4VGODMbjFbpIe.vOISth8qAaXlO7CAi4HNneqe37Jy1gGRO');
+
+        $hash->shouldReceive('make')
+            ->once()->with('foo123')
+            ->andReturn('hashedPassword');
+
+        /*
+        |------------------------------------------------------------
+        | Assertion
+        |------------------------------------------------------------
+        */
+        $this->assertTrue($validator->validatePassword($userA));
+        $this->assertFalse($validator->validatePassword($userB));
+        $this->assertTrue($validator->validatePassword($userC));
     }
 }
