43cf118cf94e024ad3c7653c9e4d295b7e2358c6
Zizaco <zizaco@gmail.com>
2014-04-19 21:14:05 -0300

    Added docblock to CacheLoginThrottleService::isThrottled method

--- src/Zizaco/Confide/CacheLoginThrottleService.php
+++ src/Zizaco/Confide/CacheLoginThrottleService.php
@@ -43,6 +43,11 @@ class CacheLoginThrottleService implements LoginThrottleServiceInterface
         return $this->countThrottle($identity);
     }
 
+    /**
+     * Tells if the given identity has reached the throttle_limit
+     * @param  mixed $identity The login identity
+     * @return boolean True if the identity has reached the throttle_limit
+     */
     public function isThrottled($identity)
     {
         $identity = $this->parseIdentity($identity);
