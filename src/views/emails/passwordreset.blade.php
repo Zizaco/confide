3bf7eb044b7bfb598e48f9f24c306c8989ff37f3
Mihail Velikov <mihailvelik@gmail.com>
2014-08-06 12:01:00 +0300

    Change  ussage to an array in email views

--- src/views/emails/passwordreset.blade.php
+++ src/views/emails/passwordreset.blade.php
@@ -1,6 +1,6 @@
 <h1>{{ Lang::get('confide::confide.email.password_reset.subject') }}</h1>
 
-<p>{{ Lang::get('confide::confide.email.password_reset.greetings', array( 'name' => $user->username)) }},</p>
+<p>{{ Lang::get('confide::confide.email.password_reset.greetings', array( 'name' => $user['username'])) }},</p>
 
 <p>{{ Lang::get('confide::confide.email.password_reset.body') }}</p>
 <a href='{{ URL::to('users/reset_password/'.$token) }}'>
