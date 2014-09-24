f00681d04ee213221544f97270390d0dceeba724
Zizaco <zizaco@gmail.com>
2014-09-24 00:04:37 -0300

    Updated views in order to avoid asking for username by default

--- src/views/emails/confirm.blade.php
+++ src/views/emails/confirm.blade.php
@@ -1,6 +1,6 @@
 <h1>{{ Lang::get('confide::confide.email.account_confirmation.subject') }}</h1>
 
-<p>{{ Lang::get('confide::confide.email.account_confirmation.greetings', array('name' => $user['username'])) }},</p>
+<p>{{ Lang::get('confide::confide.email.account_confirmation.greetings', array('name' => (isset($user['username'])) ? $user['username'] : $user['email'])) }},</p>
 
 <p>{{ Lang::get('confide::confide.email.account_confirmation.body') }}</p>
 <a href='{{{ URL::to("users/confirm/{$user['confirmation_code']}") }}}'>
--- src/views/login.blade.php
+++ src/views/login.blade.php
@@ -2,8 +2,8 @@
     <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
     <fieldset>
         <div class="form-group">
-            <label for="email">{{{ Lang::get('confide::confide.username_e_mail') }}}</label>
-            <input class="form-control" tabindex="1" placeholder="{{{ Lang::get('confide::confide.username_e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
+            <label for="email">{{{ Lang::get('confide::confide.e_mail') }}}</label>
+            <input class="form-control" tabindex="1" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
         </div>
         <div class="form-group">
         <label for="password">
