4f387ff06f7300efe669dcc6d07095531187ad4f
Dmitry Yuzhakov <dmtry88@gmail.com>
2014-12-28 20:42:22 +0700

    Fixed fullname getting is confirmation email.

--- src/views/emails/confirm.blade.php
+++ src/views/emails/confirm.blade.php
@@ -1,6 +1,6 @@
 <h1>{{ Lang::get('confide::confide.email.account_confirmation.subject') }}</h1>
 
-<p>{{ Lang::get('confide::confide.email.account_confirmation.greetings', array('name' => $user->getFullname())) }},</p>
+<p>{{ Lang::get('confide::confide.email.account_confirmation.greetings', array('name' => $user['fullname'])) }},</p>
 
 <p>{{ Lang::get('confide::confide.email.account_confirmation.body') }}</p>
 <a href='{{{ URL::to("users/confirm/{$user['confirmation_code']}") }}}'>
